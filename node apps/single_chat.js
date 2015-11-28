9064b0cd9aaa660d49a330f41e3193250bbd1a99
alafazam <alafazam@gmail.com>
2015-11-28 23:24:11 +0530

    simple chat server

--- node apps/single_chat.js
+++ node apps/single_chat.js
@@ -1,8 +1,12 @@
 var net = require('net')
 var chatServer = net.createServer()
+console.log("chat Server started");
 chatServer.on('connection', function(client) {
+    console.log("Client Connnected");
     client.write('Hi!\n');
     client.write('Bye!\n');
-    client.end()
+    client.end();
+    console.log("Client Disconnnected");
+
 })
 chatServer.listen(9000)
