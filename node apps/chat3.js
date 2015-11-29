76fa8fe6368b24caf1b5a2ee031062dcd546123c
alafazam <alafazam@gmail.com>
2015-11-30 00:09:47 +0530

    socket io

--- node apps/chat3.js
+++ node apps/chat3.js
@@ -1,10 +1,15 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  res.sendfile('index.html');
 });
 
-http.listen(3000, function() {
-    console.log('listening on *:3000');
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
 });
