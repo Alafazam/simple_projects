4dcfae8fa7c10b1efd718bde9680e688b2573785
alafazam <alafazam@gmail.com>
2015-12-01 12:54:15 +0530

    apply string format in quiz

--- node apps/simple_quiz3.js
+++ node apps/simple_quiz3.js
@@ -1,5 +1,25 @@
-var format = require('string-format');
-format.extend(String.prototype);
+var readlineSync = require('readline-sync'),
+    util = require('util');
 
-console.log('Hello, {}!'.format('Alaf'));
+question = {
+    'question': 'In Java, Which method must be implemented by all threads? :',
+    'options': ['wait()', 'start()', 'stop()', 'run()'],
+    'correctOption': 'D',
+    'correctAnswer': 'run()'
+}
 
+
+answer = readlineSync.question(ask(question), {
+    limit: ['A', 'B', 'C', 'D']
+});
+
+if (answer == question.correctOption) {
+    console.log(util.format('Yes, correct answer is %s method', question.correctAnswer));
+} else {
+    console.log(util.format('No, correct answer is %s method', question.correctAnswer));
+};
+
+
+function ask(ques) {
+    return '\n{question}:\nA : {options.0}\nB : {options.2}\nC : {options.2}\nD : {options.3}\n'.format(ques);
+}
