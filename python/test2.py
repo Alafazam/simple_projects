e31d682dea81a3d3a635560ce16624a8c0ff0929
Alaf Azam <alafazam@gmail.com>
2015-12-11 02:22:39 +0530

    selenium test

--- python/test2.py
+++ python/test2.py
@@ -2,8 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Firefox()
-driver.get("http://www.python.org")
-assert "Python" in driver.title
+driver.get("http://www.facebook.org")
+
+assert "facebook" in driver.title
 
 elem = driver.find_element_by_name("q")
 
@@ -12,5 +13,4 @@ elem.send_keys("pycon")
 elem.send_keys(Keys.RETURN)
 
 assert "No results found." not in driver.page_source
-
 driver.close()
