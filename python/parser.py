7a3ae3c64354bbba16e8d21de25cc7fa48a79265
Alaf Azam <alafazam@gmail.com>
2015-12-13 01:55:13 +0530

    add config file

--- python/parser.py
+++ python/parser.py
@@ -2,8 +2,17 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import ConfigParser
 
-# driver = webdriver.PhantomJS()
+
+config = ConfigParser.ConfigParser()
+config.readfp(open(r'config.pcgf'))
+username = config.get('credentials', 'username')
+userpassword = config.get('credentials', 'password')
+
+
+
+# # driver = webdriver.PhantomJS()
 driver = webdriver.Firefox()
 
 
@@ -15,13 +24,13 @@ driver.get("https://gmail.com")
 assert "Gmail" in driver.title
 
 
-driver.find_element_by_id('Email').send_keys("alafazam")
+driver.find_element_by_id('Email').send_keys(username)
 driver.find_element_by_id("next").click()
 
 
 WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "Passwd")) )
 
-driver.find_element_by_name('Passwd').send_keys("XXXXX")
+driver.find_element_by_name('Passwd').send_keys(userpassword)
 driver.find_element_by_id("signIn").click()
 
 
@@ -29,7 +38,6 @@ WebDriverWait(driver, 25).until(EC.presence_of_element_located((By.CLASS_NAME, "
 element = driver.find_element_by_class_name("UI")
 
 
-print element.content
-
+print element.html
 
 driver.quit()
