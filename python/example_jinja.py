7abd5550fc2cf7348e42526c32d45208af213243
Alaf Azam <alafazam@gmail.com>
2015-12-17 00:46:57 +0530

    list using jinja2

--- python/example_jinja.py
+++ python/example_jinja.py
@@ -1,4 +1,22 @@
 from jinja2 import Template
-template = Template('Hello {{ name }}!')
-p = template.render(name='John Doe')
-print p
+
+
+answers = [{
+	'url':'google.com',
+	'question':'Url for google'
+},{
+	'url':'google.com/q=',
+	'question':'qQuery Url for google'
+}]
+
+template2 = Template('''
+<ul>
+	{% for answer in answers %}
+ 		<li><a href="{{ answer.url }}">{{ answer.question }}</a></li>
+	{% endfor %}
+</ul>
+	''')
+
+p2 = template2.render(answers=answers)
+
+print p2
