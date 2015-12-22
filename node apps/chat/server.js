9a3ee53f646c19f083df446bd93ab7d20f91be83
Alaf Azam <alafazam@gmail.com>
2015-12-22 23:28:45 +0530

    chat server

--- node apps/chat/server.js
+++ node apps/chat/server.js
@@ -2,81 +2,31 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
+io.on('connection', function(socket){
+    console.log('a user connected');
 
-var connectedClients = {};
 
-io.on('connection', function(socket) {
+    
+    socket.on('chat message', function(msg){
+        io.emit('chat message', msg);
+        console.log(msg);
 
-    //added clients
-    socket.on("setClientId", function(data) {
-        connectedClients[data.id] = {
-            id: data.id, //adds key to a map
-            senderName: data.senderName
-        }
-        console.log(connectedClients);
     });
 
-    //removes clients
-    socket.on("deleteSharedById", function(data) {
-        delete connectedClients[data.id]; //removes key from map
-        socket.broadcast.emit("deleteShared", {
-            id: data.id
-        }); //send to sender
-    });
 
-    //erases canvas
-    socket.on("eraseRequestById", function(data) {
-        socket.broadcast.emit("eraseShared", {
-            id: data.id
-        });
-    });
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+  });
 
-    //returns back a list of clients to the requester
-    socket.on("getUserList", function(data) {
-        socket.emit("setUserList", connectedClients); //send to sender
-    });
 
-    //request to share
-    socket.on("requestShare", function(data) {
-        socket.broadcast.emit("createNewClient", {
-            listenerId: data.listenerId,
-            senderId: data.senderId,
-            senderName: data.senderName
-        });
-    });
 
-    //confirm did share
-    socket.on("confirmShare", function(data) {
-        socket.broadcast.emit("setConfirmShare", {
-            isSharing: data.isSharing,
-            senderId: data.senderId,
-            listenerId: data.listenerId,
-            senderName: data.senderName
-        });
-    });
-
-    //drawing data
-    socket.on('drawRequest', function(data) {
-        socket.broadcast.emit('draw', {
-            x: data.x,
-            y: data.y,
-            type: data.type,
-            isTouchDevice: data.isTouchDevice,
-            color: data.color,
-            stroke: data.stroke,
-            isLineDrawing: data.isLineDrawing,
-            isErase: data.isErase,
-            id: data.id
-        });
-    });
 
 });
 
 http.listen(3000, function(){
-  console.log('listening on *:3000');
+    console.log('listening on *:3000');
 });
