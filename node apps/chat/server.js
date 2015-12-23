12b2aa48794743e7a27abc4d4f52b951a4dd52e8
Alaf Azam <alafazam@gmail.com>
2015-12-24 01:08:23 +0530

    chat server

--- node apps/chat/server.js
+++ node apps/chat/server.js
@@ -1,18 +1,26 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var connections = [];
+
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
+    connections.push(socket);
+
     console.log('a user connected');
 
-
-    
     socket.on('chat message', function(msg){
-        io.emit('chat message', msg);
+        
+        if (msg=='connections') {
+            io.emit('chat message', connections);
+        }else{
+            io.emit('chat message', msg);
+        }
+        
         console.log(msg);
 
     });
